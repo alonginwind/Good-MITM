f95ed51803119770ff04e756c507687a7b597396
alonginwind <sunzl3721@gmail.com>
2026-03-02 17:08:43 +0800

    模拟surge环境，添加日志

--- crates/rule/src/action/js.rs
+++ crates/rule/src/action/js.rs
@@ -4,8 +4,8 @@ use hyper::{
     body::{to_bytes, Body, Bytes},
     Request,
 };
-use rquickjs::{Runtime, Context, Object, function::Func, Value, Error, TypedArray, ArrayBuffer};
-use std::{str::FromStr, rc::Rc, cell::RefCell};
+use rquickjs::{Runtime, Context, Object, function::Func, Value, Error, TypedArray, ArrayBuffer, Function};
+use std::{str::FromStr, rc::Rc, cell::RefCell, collections::HashMap};
 use rquickjs_extra_console::{Console, Formatter};
 
 macro_rules! to_js_object {
@@ -29,6 +29,10 @@ macro_rules! to_js_object {
         if let Ok(text) = String::from_utf8($body_bytes.to_vec()) {
             obj.set("body", text)?;
         }
+        // 始终提供 bodyBytes 字段（二进制数据）
+        let body_bytes = $body_bytes.to_vec();
+        let uint8_array = TypedArray::<u8>::new($ctx.clone(), body_bytes)?;
+        obj.set("bodyBytes", uint8_array)?;
 
         obj
     }};
@@ -104,14 +108,18 @@ pub async fn modify_res(code: &str, req_url: &Uri, res: Response<Body>) -> Resul
 
                 // 提取 body - 支持多种类型
                 let body: Vec<u8> = if let Ok(body_str) = obj_ref.get::<_, String>("body") {
+                    log::info!("收到字符串数据");
                     body_str.into_bytes()
-                } else if let Ok(body_binary) = obj_ref.get::<_, TypedArray<u8>>("body") {
+                } else if let Ok(body_binary) = obj_ref.get::<_, TypedArray<u8>>("bodyBytes") {
                     // TypedArray: as_bytes() 返回 Option<&[u8]>
+                    log::info!("收到TypedArray数据");
                     body_binary.as_bytes().map(|bytes| bytes.to_vec()).unwrap_or_default()
-                } else if let Ok(body_array) = obj_ref.get::<_, ArrayBuffer>("body") {
+                } else if let Ok(body_array) = obj_ref.get::<_, ArrayBuffer>("bodyBytes") {
                     // ArrayBuffer: as_bytes() 返回 Option<&[u8]>
+                    log::info!("收到ArrayBuffer数据");
                     body_array.as_bytes().map(|bytes| bytes.to_vec()).unwrap_or_default()
                 } else {
+                    log::warn!("未收到body数据");
                     Vec::new()
                 };
 
@@ -123,14 +131,54 @@ pub async fn modify_res(code: &str, req_url: &Uri, res: Response<Body>) -> Resul
             Ok(())
         });
 
+        // $persistentStore
+        let store_data: Rc<RefCell<HashMap<String, String>>> = Rc::new(RefCell::new(HashMap::new()));
+        let store_data_read = store_data.clone();
+        let read_method = Func::from(move |key: String| -> Result<String, Error> {
+            log::info!("[read] 被调用");
+            let store = store_data_read.borrow();
+            Ok(store.get(&key).cloned().unwrap_or_default())
+        });
+        let store_data_write = store_data.clone();
+        let write_method = Func::from(move |key: String, value: String| -> Result<(), Error> {
+            log::info!("[write] 被调用");
+            let mut store = store_data_write.borrow_mut();
+            store.insert(key, value);
+            Ok(())
+        });
+        // 作为方法挂载
+        let persistent_store = Object::new(ctx.clone())?;
+        persistent_store.set("read", read_method)?;
+        persistent_store.set("write", write_method)?;
+
+        // $httpClient
+        let http_client = Object::new(ctx.clone())?;
+        let get_func = Func::from(move |_options: Object, callback: Function| -> Result<(), Error> {
+            log::info!("[HTTP GET] 被调用");
+            let ctx = callback.ctx().clone();
+            let null_val = Value::new_null(ctx);
+            callback.call::<_, ()>((null_val.clone(), null_val.clone(), null_val))?;
+            Ok(())
+        });
+        http_client.set("get", get_func)?;
+
         // 注入全局变量
         let globals = ctx.globals();
         globals.set("$request", req_obj)?;
         globals.set("$response", res_obj)?;
         globals.set("$done", js_done)?;
+        globals.set("$persistentStore", persistent_store)?;
+        globals.set("$httpClient", http_client)?;
 
         // 执行 JS，得到返回值
-        let result_val: Value = ctx.eval(code)?;
+        let result_val: Value = match ctx.eval(code) {
+            Ok(v) => v,
+            Err(e) => {
+                log::error!("JS 执行失败: {:?}--异常对象: {:?}", e, ctx.catch());
+                return Ok(Response::from_parts(parts, Body::from(body_bytes)));
+            }
+        };
+
         // 优先使用 $done 回传的结果
         if let Some((headers, body)) = result_data.borrow().as_ref() {
             // headers
@@ -174,7 +222,7 @@ pub async fn modify_res(code: &str, req_url: &Uri, res: Response<Body>) -> Resul
             return Ok(Response::from_parts(parts, Body::from(body)));
         }
 
-        // JS没返回结果，也没调用$done，返回原始Response
+        log::warn!("JS 执行完成，但没有修改 Response，也没调用 $done");
         Ok(Response::from_parts(parts, Body::from(body_bytes)))
     })
 }
